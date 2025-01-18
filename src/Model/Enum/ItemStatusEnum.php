6cc6bcdd7cca32ea440f3efe58dce96396cabd5d
Bziluch <bziluch@gmail.com>
2025-01-18 19:11:01 +0100

    feat: item label in form

--- src/Model/Enum/ItemStatusEnum.php
+++ src/Model/Enum/ItemStatusEnum.php
@@ -2,10 +2,28 @@
 
 namespace App\Model\Enum;
 
-enum ItemStatusEnum: int
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+enum ItemStatusEnum: int implements TranslatableInterface
 {
     case Default = 0;
     case Offer = 1;
     case Sold = 2;
     case Archived = 3;
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Default => 'Domyślny',
+            self::Offer => 'Oferta',
+            self::Sold => 'Sprzedany',
+            self::Archived => 'Zarchiwizowany',
+        };
+    }
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return self::label();
+    }
 }
